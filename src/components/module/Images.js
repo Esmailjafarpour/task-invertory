453c7e64186eb255636e1cc5545f91377ea405ab
Esmailjafarpour <esmailjafarpour29@gmail.com>
2024-03-26 13:30:41 +0430

    responsive detail page

--- src/components/module/Images.js
+++ src/components/module/Images.js
@@ -6,25 +6,25 @@ import {
 
 const Images = ({ imageData }) => {
   return (
-    <div className="grid grid-cols-12 h-[30rem]">
+    <div className="grid grid-cols-12 h-[30rem] max-[320px]:h-[30rem] max-[375px]:h-[60rem] max-[425px]:h-[60rem]">
 
-      <div className="col-span-7">
+      <div className="col-span-7 max-[320px]:col-span-12 max-[375px]:col-span-12 max-[425px]:col-span-12">
         <img src={`${BASE_URL_Images}${imageData[0].media_src}`} className="h-[28rem] w-full"/>
       </div>
 
-      <div className="col-span-5 h-[28rem]">
+      <div className="col-span-5 max-[375px]:col-span-12 max-[425px]:col-span-12 h-[28rem] max-[320px]:hidden">
         <div className="grid grid-cols-12">
-            <div className="col-span-6">  
-                <img src={`${BASE_URL_Images}${imageData[1].media_src}`} className="h-[14rem]" />
+            <div className="col-span-6 ">  
+                <img src={`${BASE_URL_Images}${imageData[1].media_src}`} className="h-[14rem] w-full" />
             </div>
             <div className="col-span-6">  
-                <img src={`${BASE_URL_Images}${imageData[2].media_src}`} className="h-[14rem]"/>
+                <img src={`${BASE_URL_Images}${imageData[2].media_src}`} className="h-[14rem] w-full"/>
             </div>
             <div className="col-span-6">  
-                <img src={`${BASE_URL_Images}${imageData[3].media_src}`} className="h-[14rem]"/>
+                <img src={`${BASE_URL_Images}${imageData[3].media_src}`} className="h-[14rem] w-full"/>
             </div>
             <div className="col-span-6">  
-                <img src={`${BASE_URL_Images}${imageData[4].media_src}`} className="h-[14rem]"/>
+                <img src={`${BASE_URL_Images}${imageData[4].media_src}`} className="h-[14rem] w-full"/>
             </div>
         </div>
       </div>
--- src/components/template/DetailCar.js
+++ src/components/template/DetailCar.js
@@ -34,9 +34,9 @@ const DetailCar = async () => {
 
   return (
     <div className="px-5 bg-white">
-      <div className="flex px-[80px] bg-white">
-        <div className="w-200 w-1/3">
-          <div className="w-40">
+      <div className="flex-row justify-between px-[50px] max-[425px]:p-0 bg-white md:flex">
+        <div className="w-200 w-1/3 max-[320px]:w-full">
+          <div className="w-40 max-[320px]:mx-auto">
             <Link
               href="/"
               className="flex justify-center items-center justify-around text-stone-950 bg-amber-500 w-full text-sm rounded-lg py-2 font-semibold"
@@ -47,8 +47,8 @@ const DetailCar = async () => {
           </div>
         </div>
 
-        <div className="w-8/12 flex justify-end">
-          <div className="w-6/12 flex justify-around text-lg">
+        <div className="max-[1024px]:w-[45%] min-[1024px]:w-2/4 max-[768px]:w-[60%] max-[768px]:py-3 max-[425px]:w-[100%] flex justify-end lg-w-2/4">
+          <div className="w-full flex justify-between text-lg max-[320px]:flex-col max-[320px]:w-[60%] min-[320px]:mx-auto">
             <Link
               href="/"
               className="flex justify-between items-center text-yellow-400 font-semibold"
@@ -58,7 +58,7 @@ const DetailCar = async () => {
             </Link>
             <Link
               href="/"
-              className="flex justify-between items-center text-lg font-semibold"
+              className="flex justify-between items-center text-lg font-semibold max-[320px]:w-[98%] min-[320px]:px-4"
             >
               Next Vehicle
               <FaArrowCircleRight className="px-1 text-3xl" />
@@ -70,19 +70,18 @@ const DetailCar = async () => {
       <div className="py-5">
         <Images imageData={imageData} />
 
-        <div className="flex">
-
-          <div className="p-5 w-2/3">
-            <h1 className="text-4xl py-2">
+        <div className="flex max-[320px]:flex-col max-[375px]:flex-col max-[425px]:flex-col max-[768px]:flex-col">
+          <div className="p-5 w-2/3  min-[375px]:w-full min-[320px]:w-full">
+            <h1 className="text-4xl py-2 min-[375px]:text-2xl min-[320px]:text-base">
               {detailData.Vehicle.model_year} {detailData.Vehicle.make}{" "}
               {detailData.Vehicle.model} {detailData.Vehicle.BodyStyle.name}
             </h1>
-            <div className="grid py-3 grid-cols-2">
+            <div className="grid py-3 grid-cols-2 max-[425px]:grid-cols-1 max-[375px]:grid-cols-1">
               <span className="grid col-span-1">
-                VIN:{detailData.Vehicle.vin_number}
+                VIN : {detailData.Vehicle.vin_number}
               </span>
               <span className="grid col-span-1">
-                Stock#:{detailData.Vehicle.id}
+                Stock# : {detailData.Vehicle.id}
               </span>
             </div>
             <div className="w-100 h-px bg-black mt-3"></div>
@@ -90,7 +89,7 @@ const DetailCar = async () => {
               <b className="text-3xl">About Vehicle</b>
             </div>
 
-            <div className="grid grid-cols-12 gap-[4rem] justify-around px-3 my-5">
+            <div className="grid grid-cols-12 max-[320px]:grid-cols-1 max-[375px]:grid-cols-1 max-[425px]:grid-cols-2 gap-[4rem] justify-around px-3 my-5">
               <CardDetail
                 data={detailData.odometer}
                 character={"KM"}
@@ -165,10 +164,10 @@ const DetailCar = async () => {
             </div>
           </div>
 
-          <div className="w-1/3 px-3 ">
-            <div className="w-11/12 bg-slate-50 rounded-lg shadow-lg py-5 sticky top-0 right-0 z-[1000]">
+          <div className="w-1/3 px-3 max-[320px]:w-full max-[375px]:w-full max-[425px]:w-full max-[768px]:w-full">
+            <div className="w-11/12 max-[320px]:w-full max-[375px]:w-full max-[425px]:w-full bg-slate-50 rounded-lg shadow-lg py-5 sticky top-0 right-0 z-[1000] max-[320px]:relative">
               <div className="text-center pt-5">
-                <h1 className=" font-bold text-4xl pt-5">
+                <h1 className=" font-bold text-4xl pt-5 max-[1024px]:text-2xl">
                   ${sp(detailData.sell_price)}
                 </h1>
               </div>
@@ -180,32 +179,31 @@ const DetailCar = async () => {
                   <button
                     className={
                       true
-                        ? "bg-amber-500 rounded-lg w-10/12 my-2 py-[.55rem] text-white hover:bg-slate-800 hover:text-white hover:border-inherit"
-                        : "border-2 rounded-lg border-amber-500 w-10/12 my-2 py-[.55rem] hover:bg-slate-800 hover:text-white"
+                        ? "bg-amber-500 rounded-lg w-10/12 my-2 py-[.55rem] text-white hover:bg-slate-800 hover:text-white hover:border-inherit max-[1024px]:text-sm"
+                        : "border-2 rounded-lg border-amber-500 w-10/12 my-2 py-[.55rem] hover:bg-slate-800 hover:text-white max-[1024px]:text-sm"
                     }
                   >
                     Request information
                   </button>
                 </div>
                 <div className="w-full flex justify-center">
-                  <button className="border-2 rounded-sm border-amber-500 w-10/12 my-2 py-[.5rem] hover:bg-slate-800 hover:text-white hover:border-inherit">
+                  <button className="border-2 rounded-sm border-amber-500 w-10/12 my-2 py-[.5rem] hover:bg-slate-800 hover:text-white hover:border-inherit max-[1024px]:text-sm">
                     Text Us Now
                   </button>
                 </div>
                 <div className="w-full flex justify-center">
-                  <button className="border-2 rounded-sm border-amber-500 w-10/12 my-2 py-[.5rem] hover:bg-slate-800 hover:text-white hover:border-inherit">
+                  <button className="border-2 rounded-sm border-amber-500 w-10/12 my-2 py-[.5rem] hover:bg-slate-800 hover:text-white hover:border-inherit max-[1024px]:text-sm">
                     Apply For Financing
                   </button>
                 </div>
                 <div className="w-full flex justify-center">
-                  <button className="border-2 rounded-sm border-amber-500 w-10/12 my-2 py-[.5rem] hover:bg-slate-800 hover:text-white hover:border-inherit">
+                  <button className="border-2 rounded-sm border-amber-500 w-10/12 my-2 py-[.5rem] hover:bg-slate-800 hover:text-white hover:border-inherit max-[1024px]:text-sm">
                     Print Window Sticker
                   </button>
                 </div>
               </div>
             </div>
           </div>
-
         </div>
       </div>
     </div>
