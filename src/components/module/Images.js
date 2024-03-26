fe8cb00d3afbc4c163e26a4ef7d0d0fae590f75a
Esmailjafarpour <esmailjafarpour29@gmail.com>
2024-03-26 14:26:23 +0430

    responsive images page detail

--- src/components/module/Images.js
+++ src/components/module/Images.js
@@ -6,25 +6,25 @@ import {
 
 const Images = ({ imageData }) => {
   return (
-    <div className="grid grid-cols-12 h-[30rem] max-[320px]:h-[30rem] max-[375px]:h-[60rem] max-[425px]:h-[60rem]">
+    <div className="grid grid-cols-12 h-[22rem] max-[320px]:h-[18rem] max-[375px]:h-[36rem] max-[425px]:h-[44rem]">
 
-      <div className="col-span-7 max-[320px]:col-span-12 max-[375px]:col-span-12 max-[425px]:col-span-12">
-        <img src={`${BASE_URL_Images}${imageData[0].media_src}`} className="h-[28rem] w-full"/>
+      <div className="col-span-7 h-[22rem] max-[320px]:h-[18rem] max-[375px]:h-[18rem] max-[425px]:h-[22rem] max-[320px]:col-span-12 max-[375px]:col-span-12 max-[425px]:col-span-12 ">
+        <img src={`${BASE_URL_Images}${imageData[0].media_src}`} className="h-[22rem] w-full max-[320px]:h-[18rem] max-[375px]:h-[18rem]"/>
       </div>
 
-      <div className="col-span-5 max-[375px]:col-span-12 max-[425px]:col-span-12 h-[28rem] max-[320px]:hidden">
+      <div className="col-span-5 h-[22rem] max-[320px]:h-[18rem] max-[375px]:h-[18rem] max-[425px]:h-[22rem] max-[375px]:col-span-12 max-[425px]:col-span-12 h-[28rem] max-[320px]:hidden">
         <div className="grid grid-cols-12">
             <div className="col-span-6 ">  
-                <img src={`${BASE_URL_Images}${imageData[1].media_src}`} className="h-[14rem] w-full" />
+                <img src={`${BASE_URL_Images}${imageData[1].media_src}`} className="h-[11rem] w-full max-[375px]:h-[9rem] max-[320px]:h-[9rem]" />
             </div>
             <div className="col-span-6">  
-                <img src={`${BASE_URL_Images}${imageData[2].media_src}`} className="h-[14rem] w-full"/>
+                <img src={`${BASE_URL_Images}${imageData[2].media_src}`} className="h-[11rem] w-full max-[375px]:h-[9rem] max-[320px]:h-[9rem]"/>
             </div>
             <div className="col-span-6">  
-                <img src={`${BASE_URL_Images}${imageData[3].media_src}`} className="h-[14rem] w-full"/>
+                <img src={`${BASE_URL_Images}${imageData[3].media_src}`} className="h-[11rem] w-full max-[375px]:h-[9rem] max-[320px]:h-[9rem]"/>
             </div>
             <div className="col-span-6">  
-                <img src={`${BASE_URL_Images}${imageData[4].media_src}`} className="h-[14rem] w-full"/>
+                <img src={`${BASE_URL_Images}${imageData[4].media_src}`} className="h-[11rem] w-full max-[375px]:h-[9rem] max-[320px]:h-[9rem]"/>
             </div>
         </div>
       </div>
